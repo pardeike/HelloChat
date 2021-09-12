5850872f401e1fb60bec468b9eaa7ca4daeacaba
Andreas Pardeike <andreas@pardeike.net>
2021-09-12 23:11:47 +0200

    Add project files.

---
+++ Code/Data.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Threading;
+using System.Windows;
+
+namespace HelloChat
+{
+	public static class Database
+	{
+		public static ObservableCollection<ChatGroup> Chats;
+
+		static Database()
+		{
+			Chats = new();
+			Chats.Add(new ChatGroup());
+
+			var thread = new Thread(Adder);
+			thread.Start();
+		}
+
+		static void Adder()
+		{
+			while (true)
+			{
+				_ = Application.Current.Dispatcher.BeginInvoke(new Action<ChatGroup>(gr => Chats.Add(gr)), new ChatGroup());
+				Thread.Sleep(500);
+			}
+		}
+	}
+
+	/*
+	public class Chats : List<ChatGroup>, INotifyPropertyChanged, INotifyCollectionChanged
+	{
+		public event PropertyChangedEventHandler PropertyChanged;
+		public event NotifyCollectionChangedEventHandler CollectionChanged;
+
+		public Chats()
+		{
+		}
+
+		public void NotifyPropertyChanged(string propName)
+		{
+			PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propName));
+		}
+
+		public void AddAndNotify(ChatGroup element)
+		{
+			Add(element);
+			CollectionChanged?.Invoke(this, new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Add));
+		}
+
+		public void RemoveAndNotify(ChatGroup element)
+		{
+			_ = Remove(element);
+			CollectionChanged?.Invoke(this, new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Remove));
+		}
+	}
+	*/
+}
---
+++ Code/Models.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace HelloChat
+{
+	public class ChatGroup
+	{
+		private static readonly Random random = new();
+
+		public string Name { get; set; }
+		public string LatestMessageText { get; set; }
+		public DateTime LatestMessageDate { get; set; }
+		public int UnreadCount { get; set; }
+
+		public bool HasUnread => UnreadCount > 0;
+
+		public ChatGroup()
+		{
+			Name = random.Next(0, 1) == 0 ? Faker.Address.UkCounty() : Faker.Name.First() + " " + Faker.Internet.DomainWord();
+			LatestMessageText = Faker.Lorem.Sentence();
+			LatestMessageDate = Next(new DateTime(2021, 1, 1), DateTime.Now);
+			UnreadCount = random.Next(0, 20);
+		}
+
+		public static DateTime Next(DateTime start, DateTime? end = null)
+		{
+			end ??= new DateTime();
+			var range = (end.Value - start).Days;
+			return start.AddDays(random.Next(range));
+		}
+	}
+}
